df67cc6c580dfd8df18d97775c5cbdcfc6846d07
Génesis Balcazar <g.balcazar@opendeusto.es>
2024-11-17 11:18:52 +0100

    quitamos errores

--- src/main/java/com/example/demo/AuthRequest.java
+++ src/main/java/com/example/demo/AuthRequest.java
@@ -1,18 +1,12 @@
 package com.example.demo;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public class AuthRequest {
-    @Email
-    @NotBlank
+
     private String email;
 
-    @NotBlank
     private String password;
 
-    @NotBlank
     private String authProvider;
 
     public String getEmail() {
